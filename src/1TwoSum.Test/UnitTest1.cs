6fde30367f521d9d20036015addb6416e15d8f80
Kostiantyn Logunov <777koc@gmail.com>
2023-09-10 22:44:34 +0300

    added resolving for task 1 TwoSum

--- src/1TwoSum.Test/UnitTest1.cs
+++ src/1TwoSum.Test/UnitTest1.cs
@@ -10,7 +10,58 @@ namespace _1TwoSum.Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            int[] arr = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+            int[] expectedResult = new int[] { 0, 1 };
+
+            int[] result = Program.TwoSum(arr, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] arr = new int[] { 3, 2, 4 };
+            int target = 6;
+            int[] expectedResult = new int[] { 1, 2 };
+
+            int[] result = Program.TwoSum(arr, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        [Test]
+        public void Test3()
+        {
+            int[] arr = new int[] { 3, 3 };
+            int target = 6;
+            int[] expectedResult = new int[] { 0, 1 };
+
+            int[] result = Program.TwoSum(arr, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        [Test]
+        public void Test4()
+        {
+            int[] arr = new int[] { 3, 2, 3 };
+            int target = 6;
+            int[] expectedResult = new int[] { 0, 2 };
+
+            int[] result = Program.TwoSum(arr, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        [Test]
+        public void Test5()
+        {
+            int[] arr = new int[] { -1, -2, -3, -4, -5 };
+            int target = -8;
+            int[] expectedResult = new int[] { 2, 4 };
+
+            int[] result = Program.TwoSum(arr, target);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
--- src/1TwoSum/Program.cs
+++ src/1TwoSum/Program.cs
@@ -1,4 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿//Example 1:
+//Input: nums = [2, 7, 11, 15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] +nums[1] == 9, we return [0, 1].
 
-Console.WriteLine("Hello, World!");
+//Example 2:
+//Input: nums = [3, 2, 4], target = 6
+//Output: [1,2]
+
+//Example 3:
+//Input: nums = [3, 3], target = 6
+//Output: [0,1]
+
+using System;
+using System.Collections.Generic;
+
+int[] arr1 = new int[] { 2, 7, 11, 15 };
+int target1 = 9;
+
+int[] arr2 = new int[] { 3, 2, 4 };
+int target2 = 6;
+
+int[] arr3 = new int[] { 3, 3 };
+int target3 = 6;
+
+int[] arr4 = new int[] { 3, 2, 3 };
+int target4 = 6;
+
+int[] arr5 = new int[] { -1, -2, -3, -4, -5 };
+int target5 = -8;
+
+int[] result = TwoSum(arr1, target1);
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
+
+public static partial class Program
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        List<int> result = new List<int>();
+
+        bool loop = true;
+        int startInx = 0;
+        int i = 1;
+        int cycle = 1;
+
+        while (loop && startInx < nums.Length-1 ) {
+            
+            if (nums[startInx] + nums[startInx+i] == target)
+            {
+                loop = false;
+                result.Add(startInx);
+                result.Add(startInx+i);
+            }
+
+            if (i == nums.Length - cycle)
+            {
+                startInx++;
+                i = 1;
+                cycle++;
+                continue;
+            }
+
+            i++;
+        }
+
+        return result.ToArray();
+    }
+}
