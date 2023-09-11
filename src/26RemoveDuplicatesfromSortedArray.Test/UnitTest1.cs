f552240950147f5250fff45dc2586221d932790a
Kostiantyn Logunov <777koc@gmail.com>
2023-09-11 18:33:41 +0300

    added solution for task 26 Remove Duplicates from Sorted Array

--- src/26RemoveDuplicatesfromSortedArray.Test/UnitTest1.cs
+++ src/26RemoveDuplicatesfromSortedArray.Test/UnitTest1.cs
@@ -10,7 +10,25 @@ namespace _26RemoveDuplicatesfromSortedArray.Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            int[] arr = new int[] { 1, 1, 2 };
+            int expectedCount = 2;
+            int[] expectedResult = new int[] { 1, 2};
+
+            int count = Program.RemoveDuplicates(arr);
+
+            Assert.That(count, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] arr = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int expectedCount = 5;
+            int[] expectedResult = new int[] { 0, 1, 2, 3, 4 };
+
+            int count = Program.RemoveDuplicates(arr);
+
+            Assert.That(count, Is.EqualTo(expectedCount));
         }
     }
 }
--- src/26RemoveDuplicatesfromSortedArray/Program.cs
+++ src/26RemoveDuplicatesfromSortedArray/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int[] nums = new int[] { 1, 1, 2 };
+int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+Console.WriteLine(RemoveDuplicates(nums));
+Console.WriteLine("-----------------------------");
+
+foreach (var item in nums)
+{
+    Console.WriteLine(item);
+}
+
+int RemoveDuplicates(int[] nums)
+{
+    int k = 1;
+    int x = 1;
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i-1])
+        {
+            nums[x] = nums[i];
+            k++;
+            x++;
+        }
+    }
+
+    return k;
+}
+
+// first stupid resolving
+//int RemoveDuplicates(int[] nums)
+//{
+//    int[] result = new int[nums.Length];
+//    int k = 1;
+//    result[0] = nums[0];
+//    int x = 0;
+
+//    for (int i = 1; i < nums.Length; i++)
+//    {
+//        if (result[x] != nums[i])
+//        {
+//            k++;
+//            x++;
+//            result[x] = nums[i];
+//        }
+//    }
+
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        nums[i] = result[i];
+//    }
+
+//    return k;
+//}
+
+public static partial class Program
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int k = 1;
+        int x = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[x] = nums[i];
+                k++;
+                x++;
+            }
+        }
+
+        return k;
+    }
+}
